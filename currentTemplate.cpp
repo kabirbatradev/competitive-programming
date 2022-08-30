811b1ceca9292f05146586619d6299e84f46cc4b
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2022-08-30 15:19:32 -0700

    added my favorite template

---
+++ currentTemplate.cpp
@@ -0,0 +1,27 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+// const int dx[] = {0,0,1,-1}, dy[] = {1,-1,0,0};
+// ll is kinda op pog use it ALWAYS
+// #define PI 3.1415926535897932384626433832795
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+  // cout.precision(10);
+
+  int n;
+  cin >> n;
+
+  // int N = (int)1e5;
+  // vector<int> nums;
+  // nums.reserve(N);
+
+  for (int tc = 0; tc < n; tc++) {
+
+  }
+  
+}

d00cb02ef39eee8044e3620868dc552752a4c199
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2022-11-01 22:28:08 -0400

    completed find eventual safe states

---
+++ leetCode/findEventualSafeStates2.cpp
@@ -0,0 +1,94 @@
+// this solution is faster because it does not require a sort
+// (O(nlogn) --> O(n) for last step)
+
+class Solution {
+public:
+    vector<int> nodeStates;
+    vector<int> safeNodes;
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+        int n = graph.size();
+        
+        nodeStates.reserve(n);
+        
+        // node states:
+            // -1: not visited
+            // 0: safe
+            // 1: not safe
+            // 2: visited
+        
+        for (int i = 0; i < n; i++) {
+            nodeStates.push_back(-1);
+        }
+        
+        
+        for (int i = 0; i < n; i++) {
+            // if not visited, then DFS from that node
+            if (nodeStates.at(i) == -1) {
+                DFS(i, graph);
+            }
+        }
+        
+        // for (int i = 0; i < graph.size(); i++) {
+        //     cout << i << " " << nodeStates.at(i) << endl;
+        // }
+
+        
+        // return array of all safe nodes
+        // sort(safeNodes.begin(), safeNodes.end());
+        for (int i = 0; i < n; i++) {
+            if (nodeStates.at(i) == 0) {
+                safeNodes.push_back(i);
+            }
+        }
+        return safeNodes;
+        
+        
+    }
+    
+    void DFS(int newNode, vector<vector<int>>& graph) {
+        nodeStates.at(newNode) = 2;
+        
+        // iterate through adjacency list
+        vector<int> adjList = graph.at(newNode);
+        
+        bool areWeSafe = true;
+        
+        for (int i = 0; i < adjList.size(); i++) {
+            int adjNode = adjList.at(i);
+            if (nodeStates.at(adjNode) == -1) {
+                // not visited
+                // then DFS this node and check what it ends up being
+                DFS(adjNode, graph);
+            }
+            
+            
+            if (nodeStates.at(adjNode) == 0) {
+                // safe
+                // then we are safe so far
+                continue;
+            }
+            else if (nodeStates.at(adjNode) == 1) {
+                // not safe
+                areWeSafe = false;
+                break;
+            }
+            else if (nodeStates.at(adjNode) == 2) {
+                // already visited
+                areWeSafe = false;
+                break;
+            }
+        }
+        
+        if (areWeSafe) {
+            nodeStates.at(newNode) = 0;
+            // safeNodes.push_back(newNode);
+        }
+        else {
+            nodeStates.at(newNode) = 1;
+        }
+        
+        
+    }
+};

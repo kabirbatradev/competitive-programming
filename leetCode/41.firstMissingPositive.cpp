1d657763b17b0907f52232e50d069347634e6092
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-03-22 12:05:50 -0400

    Update 41.firstMissingPositive.cpp

--- leetCode/41.firstMissingPositive.cpp
+++ leetCode/41.firstMissingPositive.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        unordered_set<int> seen;
-        for (int num : nums) {
-            seen.insert(num);
-        }
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        unordered_set<int> seen(nums.begin(), nums.end());
+        // for (int num : nums) {
+        //     seen.insert(num);
+        // }
         int i = 1;
-        while (true) {
-            if (seen.find(i) == seen.end()) {
-                // cout << i << "\n";
-                return i;
-            }
+        while (seen.find(i) != seen.end()) {
+            // if (seen.find(i) == seen.end()) {
+            //     // cout << i << "\n";
+            //     return i;
+            // }
             i++;
         }
+        return i;
         
     }
 };
+
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         unordered_set<int> seen;
+//         for (int num : nums) {
+//             seen.insert(num);
+//         }
+//         int i = 1;
+//         while (true) {
+//             if (seen.find(i) == seen.end()) {
+//                 // cout << i << "\n";
+//                 return i;
+//             }
+//             i++;
+//         }
+        
+//     }
+// };

dfe0dfeb6122cfc3e51592bc6c7cbc9bd94db6a5
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-04-01 15:21:23 -0400

    Create simpleTutorial.cpp

---
+++ codeforces/simpleTutorial.cpp
@@ -0,0 +1,28 @@
+
+// https://codeforces.com/group/cqtHHtQmin/contest/436435/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+// const int dx[] = {0,0,1,-1}, dy[] = {1,-1,0,0};
+// ll is kinda op pog use it ALWAYS
+// #define PI 3.1415926535897932384626433832795
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+  // cout.precision(10);
+
+  string s; cin >> s;
+  ll n; cin >> n;
+  ll a, b; cin >> a >> b;
+
+  for (int i = 0; i < n; i++) {
+    cout << s;
+  }
+  cout << "\n" << a * b;
+
+  
+}

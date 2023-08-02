0202a04cb5b5e3c4f559bb5a34c75b42da7d592f
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-08-01 20:05:50 -0700

    Faster solution to 77.Combinations.cpp

--- leetCode/medium/77.Combinations.cpp
+++ leetCode/medium/77.Combinations.cpp
@@ -4,26 +4,23 @@ public:
     vector<vector<int>> combine(int n, int k) {
         solution = vector<vector<int>>();
         
-        queue<int> remainingChoicesList;
-        for (int i = 1; i <= n; i++) remainingChoicesList.push(i);
         int remainingChoices = k;
-        traverse(vector<int>(), remainingChoicesList, remainingChoices);
+        traverse(vector<int>(), n, 1, remainingChoices);
 
         return solution;
     }
 
-    void traverse(vector<int> path, queue<int> remainingChoicesList, int remainingChoices) {
+    void traverse(vector<int> path, int n, int nStartIndex, int remainingChoices) {
         if (remainingChoices == 0) {
             solution.push_back(path);
             return;
         }
 
-        while (!remainingChoicesList.empty()) {
-            int choice = remainingChoicesList.front(); // pops out smaller numbers first
-            remainingChoicesList.pop();
-
-            path.push_back(choice);
-            traverse(path, remainingChoicesList, remainingChoices - 1); // decrement remaining choices
+        for (int i = nStartIndex; i <= n; i++) {
+            path.push_back(i);
+            traverse(path, n, i+1, remainingChoices - 1); 
+            // next traverse should start from after the element added to the path
+            // decrement remaining choices
             path.pop_back();
 
         }

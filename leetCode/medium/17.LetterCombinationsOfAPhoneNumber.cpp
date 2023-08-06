4dd477f18f8b5df9461ae5f161bf91ac589b07f1
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-08-05 18:24:17 -0700

    remove debug comments, update code

--- leetCode/medium/17.LetterCombinationsOfAPhoneNumber.cpp
+++ leetCode/medium/17.LetterCombinationsOfAPhoneNumber.cpp
@@ -2,39 +2,49 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
 
+        // empty string edge case
+        if (digits.length() == 0) {
+            return vector<string>();
+        }
+
         vector<string> combinations;
 
-        unordered_map<char, string> digit_to_letters = {
-            {'2', "abc"},
-            {'3', "def"},
-            {'4', "ghi"},
-            {'5', "jkl"},
-            {'6', "mno"},
-            {'7', "pqrs"},
-            {'8', "tuv"},
-            {'9', "wxyz"}
+        // unordered_map<char, string> digit_to_letters = {
+        //     {'2', "abc"},
+        //     {'3', "def"},
+        //     {'4', "ghi"},
+        //     {'5', "jkl"},
+        //     {'6', "mno"},
+        //     {'7', "pqrs"},
+        //     {'8', "tuv"},
+        //     {'9', "wxyz"}
+        // };
+
+        // arrays should be faster than hashmap
+        string digit_to_letters[] = {
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz",
         };
+    
+        // given at least 1 digit, start with empty string
+        combinations.push_back("");
 
+        // for every digit, update the list of combinations
         for (char digit : digits) {
-
-            if (combinations.empty()) {
-                combinations.push_back("");
-            }
-
-            // // digit to letter
-            // int num = digit - '2'; // 2 -> 0, 3 -> 1
             
-            // // first letter corresponding to that number
-            // char letter = num*3 + 'a'; 
-
+            // replace combinations with new longer combinations
             vector<string> newCombinations;
             for (string combo : combinations) {
-                // for (int i = 0; i < 3; i++) {
-                //     string newCombo(combo);
-                //     newCombo.push_back(letter + i);
-                //     newCombinations.push_back(newCombo);
-                // }
-                for (char letter : digit_to_letters[digit]) {
+                // for each old combo, add 3 or 4 new combos
+
+                // - '2' to offset for array ('2' -> 0 index)
+                for (char letter : digit_to_letters[digit - '2']) {
                     string newCombo(combo);
                     newCombo.push_back(letter);
                     newCombinations.push_back(newCombo);

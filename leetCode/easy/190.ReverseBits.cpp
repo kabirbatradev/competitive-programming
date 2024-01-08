7c0bf52a9d3aad8fec0563112b8f7eaf579b1a6a
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2024-01-07 21:37:08 -0500

    faster and less memory

--- leetCode/easy/190.ReverseBits.cpp
+++ leetCode/easy/190.ReverseBits.cpp
@@ -4,13 +4,10 @@ public:
 
         uint32_t ret = 0;
 
-        uint32_t singleBit = 0;
         for (int i = 0; i < 32; i++) {
-            singleBit = n & 1; // extract the right most bit
-            n = n >> 1; // shift n to the right by 1
-
             ret = ret << 1; // shift ret left one to make a empty spot on the right
-            ret = ret | singleBit; // write the single bit to the rightmost spot
+            ret = ret | (n&1); // write the rightmost bit of n to the rightmost spot
+            n = n >> 1; // shift n to the right by 1 (deleting the right most bit)
         }
 
         return ret;

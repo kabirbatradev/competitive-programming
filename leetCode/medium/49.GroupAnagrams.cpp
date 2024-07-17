973347e0d49e0ebbc78a9222db504989d48bddc6
Kabir Batra <kabirbatraa@gmail.com>
2024-07-16 22:07:27 -0700

    updated solution was supposed to be m*n it wouldve worked if this was python

--- leetCode/medium/49.GroupAnagrams.cpp
+++ leetCode/medium/49.GroupAnagrams.cpp
@@ -2,9 +2,46 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
+        // solution 2:
+        // hash the characters in the string instead of sorting
+        // then make a hash table of the hashed strings
+        // we cannot hash maps but we can hash a count array
+
+        // count array for each string: array of size 26 of counts where the first entry is the letter a
+        // table<key = int[26], value = list of original grouped strings>
+
+        // it would have worked in python with a tuple gg
+        unordered_map<int[26], vector<string>> groups;
+
+        for (string s : strs) {
+            int counts[26] = {0};
+
+            for (char c : s) {
+                int index = c - 'a';
+                counts[index]++;
+            }
+
+            if (groups.count(counts) == 0) {
+                groups[counts] = {s};
+            }
+            else {
+                groups[counts].push_back(s);
+            }
+
+        }
+        vector<vector<string> ret;
+        for (auto pair : groups) {
+            vector<string> group = pair.second;
+            ret.push_back(group);
+        }
+
+        return ret;
+
+
+
         // solution 1: 
         // sort each string (the lengths are quite short)
-        // then put them all in a map<key = sorted string, value = list of all of the solutions
+        // then put them all in a map<key = sorted string, value = list of all of the solutions>
         // runtime: nlog(n) * m
 
         unordered_map<string, vector<string>> groups;
@@ -27,6 +64,8 @@ public:
         }
 
         return ret;
+
+        
 
     }
 };

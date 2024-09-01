9f54fb92ebb49af7a1246101fa310de31477ad7e
Kabir Batra <kabirbatraa@gmail.com>
2024-09-01 17:28:55 -0400

    Create cleanTemplate.cpp

---
+++ cleanTemplate.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+  cin.exceptions(cin.failbit);
+
+  int t; cin >> t;
+  for (int tc = 0; tc < t; tc++) {
+
+  }
+}

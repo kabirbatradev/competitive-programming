400b2c2cc438115a187e3c54ec9c8647879878ed
Kabir Batra <kabirbatraa@gmail.com>
2024-12-23 19:35:25 -0800

    Update 79. Word Search.cpp

--- leetCode/medium/79. Word Search.cpp
+++ leetCode/medium/79. Word Search.cpp
@@ -1,68 +1,49 @@
 class Solution {
 public:
-
-    // global visited array (represents if something is already a part of the current word)
-    vector<vector<bool>> visited;
-
     bool exist(vector<vector<char>>& board, string word) {
-        // looks like basic backtracking
-        // no major optimizations i can think of
-
+        vector<vector<bool>> visited(board.size(), vector<bool>(board[0].size(), false));
         for (int i = 0; i < board.size(); i++) {
-            visited.push_back({});
             for (int j = 0; j < board[0].size(); j++) {
-                visited[i].push_back(false);
+                if (board[i][j] == word[0]) {
+                    // visited[i][j] = true;
+                    if (dfs(i, j, 1, board, visited, word)) {
+                        return true;
+                    }
+                    // visited[i][j] = false;
+                }
             }
         }
 
-        // for every position on the board, try doing dfs
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-
-                if(dfs(i, j, board, word, 0)) return true;
-                
-            }
-        }
-
-        // tried starting from every position but it all failed
         return false;
-        
     }
 
     int dx[4] = {-1, 1, 0, 0};
     int dy[4] = {0, 0, -1, 1};
 
-    bool dfs(int i, int j, vector<vector<char>>& board, string& word, int index) {
-
-        
-
-        // if ij is in a visited part of the current word, then cannot continue in that direction
-        // if (visited[i][j]) return false;
-
-        // if the letter at ij does not match the word at index, then fail
-        if (board[i][j] != word[index]) return false;
-
-        // if this character was the last one, then yay we win
-        if (index == word.size()-1) return true;
-        
-        // continue dfs in all directions, maintain visited array
-        // set current position as visited
+    bool dfs(int i, int j, int charPos, 
+        vector<vector<char>>& board, vector<vector<bool>>& visited,
+        string& word
+    ) {
+        if (charPos == word.length()) return true;
         visited[i][j] = true;
+
         for (int k = 0; k < 4; k++) {
-            int newi = i + dx[k];
-            int newj = j + dy[k];
+            int i2 = i + dx[k];
+            int j2 = j + dy[k];
 
-            // make sure newi and newj are not out of range
-            if (newi < 0 || newi == board.size() || newj < 0 || newj == board[0].size()) continue;
+            // out of bounds or visited already or character on board does not match
+            if (i2 < 0 || j2 < 0 || i2 >= board.size() || j2 >= board[0].size()
+            || visited[i2][j2] || board[i2][j2] != word[charPos]) {
+                continue;
+            }
 
-            // make sure new is not already visited (skip if is)
-            if (visited[newi][newj]) continue;
-
-            if (dfs(newi, newj, board, word, index+1)) return true;
+            if (dfs(
+                i2, j2, charPos+1, board, visited, word
+            )) return true;
         }
-        // undo set current position as visited
-        visited[i][j] = false;
 
-        return false; // tried all 4 directions but they all failed
+        visited[i][j] = false;
+        return false;
+
     }
 };

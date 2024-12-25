42e8a86d92de721ba89a0061840cc4d0f1126dec
Kabir Batra <kabirbatraa@gmail.com>
2024-12-25 02:41:02 -0800

    slightly faster solution to 3. Longest Substring Without Repeating Characters

--- leetCode/medium/3.LongestSubstringWithoutRepeatingCharacters.cpp
+++ leetCode/medium/3.LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
-        int longestLength = 0;
-        unordered_set<char> windowLetters;
+        // sliding window, use set to keep track of letters in use
+        // increase window size if new letter is not in set
+        // if in use, remove from back of window until that char is no longer in use
 
-        int windowLeftIndex = 0;
-        for (int i = 0; i < s.length(); i++) {
+        if (s.length() == 0) return 0;
 
-            if (windowLetters.find(s.at(i)) != windowLetters.end()) {
-                // is in the set:
+        int best = 1;
+        unordered_set<char> inUse;
+        int windowStart = 0;
+        // int windowEnd = 0; // inclusive 
+        inUse.insert(s[0]);
 
-                // keep removing from the left side of the window 
-                // until the new letter is removed
-                bool done = false;
-                while (!done) {
-                    if (s.at(i) == s.at(windowLeftIndex)) {
-                        done = true;
+        for (int i = 1; i < s.length(); i++) {
+            char c = s[i];
+
+            if (inUse.count(c)) {
+                // remove from back of window
+                while (true) {
+                    if (s[windowStart] == c) {
+                        windowStart++;
+                        // inUse.erase(c);
+                        break;
                     }
-                    windowLetters.erase(s.at(windowLeftIndex));
-                    windowLeftIndex++;
+                    else {
+                        inUse.erase(s[windowStart]);
+                        windowStart++;
+                    }
                 }
+                continue; // no chance that this window is best, so just remove the "erase"
             }
 
-            // now it is guaranteed to not be in the set
-            windowLetters.insert(s.at(i));
+            // insert into set
+            inUse.insert(c);
+            // windowEnd++;
+            best = max(best, (int)inUse.size());
 
-            // if new longest then update
-            if (windowLetters.size() > longestLength) {
-                longestLength = (int)windowLetters.size();
-            }
         }
 
-        return longestLength;
+        return best;
     }
 };

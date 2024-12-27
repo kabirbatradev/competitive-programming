f9edbb9f6cf9bf5d6283c32fb3bba315107ed3f8
Kabir Batra <kabirbatraa@gmail.com>
2024-12-26 16:06:31 -0800

    cleaner solution to 102. Binary Tree Level Order Traversal

--- leetCode/medium/102. Binary Tree Level Order Traversal.cpp
+++ leetCode/medium/102. Binary Tree Level Order Traversal.cpp
@@ -12,46 +12,34 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        using Node = TreeNode; // I forgot its called TreeNode instead of Node oops
+        using Node = TreeNode;
 
         if (root == nullptr) return {};
 
-        vector<vector<int>> ret(1, vector<int>()); // initialize the first level to an empty list
+        // initialize the first level to an empty list
+        vector<vector<int>> ret; 
 
-        // alterating queues idea: always push to the other queue
-            // when queue is empty, then that level is complete
-
-        bool usingQueue0 = true;
-
-        queue<Node *> q0;
-        queue<Node *> q1;
-
-        q0.push(root);
+        queue<Node *> q;
+        q.push(root);
         
-        while (true) {
+        while (!q.empty()) {
+            
+            int count = q.size(); // nodes in this level
+            // ret.push_back({});
+            ret.emplace_back(count); // creates a vector of the known size
 
-            queue<Node *>& q = usingQueue0 ? q0 : q1;
-            queue<Node *>& otherQ = usingQueue0 ? q1 : q0;
+            for (int i = 0; i < count; i++) {
 
-            while (!q.empty()) {
                 Node* current = q.front();
                 q.pop();
 
-                ret.back().push_back(current->val); // push val
-                // push children to other queue
-                if (current->left != nullptr) otherQ.push(current->left);
-                if (current->right != nullptr) otherQ.push(current->right);
+                ret.back()[i] = current->val; // put val in the last level 
+                // push children if they exist
+                if (current->left != nullptr) q.push(current->left);
+                if (current->right != nullptr) q.push(current->right);
+
             }
 
-            // now queue is empty
-
-            // if the other queue is also empty, then we are done
-            if (otherQ.empty()) break;
-
-            // otherwise, there are still nodes in the other level
-            
-            ret.push_back({}); // add new level to ret
-            usingQueue0 = !usingQueue0; // toggle which queue is being used
         }
 
         return ret;

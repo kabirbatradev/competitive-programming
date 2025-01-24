156f51cbefa23785cdacb25d5a69176bfdbc48de
Kabir Batra <kabirbatraa@gmail.com>
2025-01-23 19:42:59 -0500

    Create Baloni.cpp

---
+++ kattis/cp1 again/Topic 1 Useful Data Structures and Complete Search/Baloni.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+  cin.exceptions(cin.failbit);
+
+  int arrowsShot = 0;
+
+  // counting map for counting the current arrows still flying
+  unordered_map<int, int> heights;
+
+  int n; cin >> n;
+  for (int i = 0; i < n; i++) {
+    int h; cin >> h;
+
+    // arrow already exists, hit balloon, so move arrow down one
+    if (heights[h] > 0) {
+      heights[h]--;
+      heights[h-1]++;
+    }
+    // need to shoot new arrow at that balloon and then the arrow drops
+    else {
+      heights[h-1]++;
+      arrowsShot++;
+    }
+  }
+
+  // note: the arrows shot for past balloons 
+  // can be used immediately for future balloons
+
+  cout << arrowsShot << '\n';
+}

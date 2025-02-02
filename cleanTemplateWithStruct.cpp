db85cba009c798c6d6151fd7ae2497e2df1a1a1e
Kabir Batra <kabirbatraa@gmail.com>
2025-02-01 19:00:26 -0500

    create new template with struct
    no longer need to forward declare functions if they are all in a class

--- cleanTemplateWithStruct.cpp
+++ cleanTemplateWithStruct.cpp
@@ -1,16 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
+
+struct Solution {
+  Solution() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+    }
+  }
+};
 
 int main() {
   std::ios::sync_with_stdio(false);
   cin.tie(0); cout.tie(0);
   cin.exceptions(cin.failbit);
-
-  int n; cin >> n;
-
-  for (int i = 0; i < n; i++) {
-    
-  }
+  Solution s;
 }

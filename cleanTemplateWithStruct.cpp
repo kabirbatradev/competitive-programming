9d6700883cf62df7a6ce71153f445f411e72cfb8
Kabir Batra <kabirbatraa@gmail.com>
2025-02-01 19:05:15 -0500

    update templates

--- cleanTemplateWithStruct.cpp
+++ cleanTemplateWithStruct.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 using ll = long long;
 
 struct Solution {
   Solution() {
-    int n;
-    cin >> n;
+    int n; cin >> n;
 
     for (int i = 0; i < n; i++) {
     }

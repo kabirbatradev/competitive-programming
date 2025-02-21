49328bf0d2e7964091c592540b2463949c643a31
Kabir Batra <kabirbatraa@gmail.com>
2025-02-20 19:49:26 -0500

    update templates

--- cleanTemplate.cpp
+++ cleanTemplate.cpp
@@ -6,8 +6,9 @@ int main() {
   std::ios::sync_with_stdio(false);
   cin.tie(0); cout.tie(0);
   cin.exceptions(cin.failbit);
-  // cout.precision(10);
-
+  // cout.precision(1); // # decimal places
+  // cout << fixed; // force precision decimal places
+  
   int n; cin >> n;
 
   for (int i = 0; i < n; i++) {
--- cleanTemplateWithStruct.cpp
+++ cleanTemplateWithStruct.cpp
@@ -15,6 +15,7 @@ int main() {
   std::ios::sync_with_stdio(false);
   cin.tie(0); cout.tie(0);
   cin.exceptions(cin.failbit);
-  // cout.precision(10);
+  // cout.precision(1); // # decimal places
+  // cout << fixed; // force precision decimal places
   Solution s;
 }

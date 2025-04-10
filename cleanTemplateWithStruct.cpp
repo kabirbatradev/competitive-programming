6a835626e4993d9c2792d8b0dbb8d4ead9b9d5a6
Kabir Batra <kabirbatraa@gmail.com>
2025-04-10 05:40:20 -0400

    update templates and names

--- cleanTemplateWithStruct.cpp
+++ cleanTemplateWithStruct.cpp
@@ -1,8 +1,5 @@
-// Author: Kabir Batra
-// It is okay to share my code for educational purposes
-
 #include <bits/stdc++.h>
-#define int long long // replace all "int" with "long long"
+#define int long long
 using namespace std;
 
 struct Solution {
@@ -15,12 +12,11 @@ struct Solution {
   }
 };
 
-// using "signed" works around "int" being replaced with long long
 signed main() {
-  std::ios::sync_with_stdio(false);
-  cin.tie(0); cout.tie(0);
-  // cin.exceptions(cin.failbit); // idk what this does but it prevents me from doing while (cin >> n)
-  // cout.precision(1); // # decimal places
-  // cout << fixed; // force precision decimal places
-  Solution s;
+  std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  // cout.precision(1); cout << fixed; // # decimal places
+
+  // int t; cin >> t;
+  // for (int i = 0; i < t; i++) 
+    Solution s;
 }
---
+++ templateWithStruct.cpp
@@ -0,0 +1,26 @@
+// Author: Kabir Batra
+// It is okay to share my code for educational purposes
+
+#include <bits/stdc++.h>
+#define int long long // replace all "int" with "long long"
+using namespace std;
+
+struct Solution {
+  Solution() {
+    int n; cin >> n;
+
+    for (int i = 0; i < n; i++) {
+
+    }
+  }
+};
+
+// using "signed" works around "int" being replaced with long long
+signed main() {
+  std::ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+  // cin.exceptions(cin.failbit); // idk what this does but it prevents me from doing while (cin >> n)
+  // cout.precision(1); // # decimal places
+  // cout << fixed; // force precision decimal places
+  Solution s;
+}
